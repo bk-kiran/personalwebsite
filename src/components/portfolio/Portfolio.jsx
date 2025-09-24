a98c25859e7bc55a75bcc200be960da6468b65c3
bk-kiran <kiranbk1704@gmail.com>
2025-09-24 19:02:41 -0400

    updated projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -18,6 +18,49 @@ const Portfolio = () => {
 
         <div className='project1'>
             <img className='project1screenshot' src={vgamefinder}/>
+            <h1 className='project1title'>Fantasy Premier League Player Finder ⚽🔍</h1>
+            <br></br>
+            <p className='project1caption'>Search, compare, and analyze 500+ soccer players with ease. Featuring an interactive React.js frontend with real-time filters and a Spring Boot + PostgreSQL backend, it handles 5K+ concurrent users while ensuring sub-500ms response times through optimized queries and RESTful APIs.</p>
+            <br></br>
+            <p><span className='projecttag1'>#springboot</span>&nbsp;&nbsp;&nbsp;<span className='projecttag2'>#reactjs</span>&nbsp;&nbsp;&nbsp;<span className='projecttag3'>#postgresql</span></p>
+            <br></br>
+            <p><a className='viewcodedemo' href="https://github.com/bk-kiran/FantasyPL-DataHub-24-25-" target='__blank'>Code</a>&nbsp;&nbsp;👨‍💻&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a className='viewcodedemo' href="https://vgamefinder.kiranbk.com/" target='__blank'>Live Demo</a>&nbsp;&nbsp;🖥️</p>
+        </div>
+
+        <div className='project2'>
+            <img className='project2screenshot' src={amazonproject}/>
+            <h1 className='project2title'>Finance Hive 💰</h1>
+            <p className='project2caption'>An AI-powered personal finance assistant app for iOS that blends budgeting, stock tracking, simulations, and gamified learning. Built with Swift + SwiftUI in an MVVM architecture, Finance Hive features Finbot (AI assistant), a personalized dashboard, and community-driven challenges to make finance approachable and fun.</p>
+            <br></br>
+            <p><span className='projecttag4'>#swift</span>&nbsp;&nbsp;&nbsp;<span className='projecttag5'>#mvvm</span>&nbsp;&nbsp;&nbsp;<span className='projecttag6'>#ai</span></p>
+            <br></br>
+            <p><a className='viewcodedemo' href="https://github.com/Saipulavarthy/FinanceHive" target='__blank'>Code</a>&nbsp;&nbsp;👨‍💻&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a className='viewcodedemo' href="#" target='__blank'>Live Demo</a>&nbsp;&nbsp;🖥️</p>
+        </div>
+
+        <div className='project1'>
+            <img className='project1screenshot' src={vgamefinder}/>
+            <h1 className='project1title'>AI Resume Analyzer 📑🤖</h1>
+            <br></br>
+            <p className='project1caption'>Ace the resume screen with this AI-powered analyzer! Built with React.js and Claude APIs, it achieves 92% ATS accuracy and reduces feedback latency by 40% through prompt-engineered NLP pipelines. Securely processes 100+ resumes in parallel with Puter.js serverless auth and encrypted storage, delivering faster and more consistent feedback.</p>
+            <br></br>
+            <p><span className='projecttag7'>#react</span>&nbsp;&nbsp;&nbsp;<span className='projecttag1'>#nlp</span>&nbsp;&nbsp;&nbsp;<span className='projecttag3'>#puterjs</span></p>
+            <br></br>
+            <p><a className='viewcodedemo' href="https://github.com/bk-kiran/ai_resume_analyzer" target='__blank'>Code</a>&nbsp;&nbsp;👨‍💻&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a className='viewcodedemo' href="https://vgamefinder.kiranbk.com/" target='__blank'>Live Demo</a>&nbsp;&nbsp;🖥️</p>
+        </div>
+
+        <div className='project2'>
+            <img className='project2screenshot' src={amazonproject}/>
+            <h1 className='project2title'>Summafy 📰</h1>
+            <br></br>
+            <p className='project2caption'>Stay informed in seconds. Summafy is a clean Chrome extension that extracts the main content from any article and generates concise AI-powered summaries using Google Gemini. Designed with a focus on clarity and usability, it transforms long reads into quick takeaways.</p>
+            <br></br>
+            <p><span className='projecttag5'>#javascript</span>&nbsp;&nbsp;&nbsp;<span className='projecttag7'>#geminiai</span>&nbsp;&nbsp;&nbsp;<span className='projecttag2'>#chromewebextension</span></p>
+            <br></br>
+            <p><a className='viewcodedemo' href="https://github.com/bk-kiran/Summafy" target='__blank'>Code</a>&nbsp;&nbsp;👨‍💻&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a className='viewcodedemo' href="#" target='__blank'>Live Demo</a>&nbsp;&nbsp;🖥️</p>
+        </div>
+
+        <div className='project1'>
+            <img className='project1screenshot' src={vgamefinder}/>
             <h1 className='project1title'>VGameFinder 2.0 🕹️</h1>
             <br></br>
             <p className='project1caption'>Looking for your next video game adventure? Use VGameFinder 2.0 to discover over 3,000 game entries and recieve personalized recommendations based on your previous plays! The front-end application was architected using ReactJS, Tailwind CSS and the RAWG API. Whereas, the game recommendation system integrated pandas dataframes and scikit-learn's cosine
@@ -59,21 +102,12 @@ similarity algorithm.</p>
             <br></br>
             <p><a className='viewcodedemo' href="https://github.com/bk-kiran/recipefinder" target='__blank'>Code</a>&nbsp;&nbsp;👨‍💻&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://drive.google.com/file/d/1UNgvQHlVNIBIbcb2FwobRH0bASfIZlQ7/view?usp=sharing" target='__blank' className='viewcodedemo'>Live Demo</a>&nbsp;&nbsp;🖥️</p>
         </div>
+
         <div className='project1'>
-            <img className='project1screenshot' src={videogameproject}/>
-            <h1 className='project1title'>Video Game Recommender 🎮</h1>
+            <img className='project1screenshot' src={djproject}/>
+            <h1 className='project1title'>Predicting Stock Prices 📈</h1>
             <br></br>
-            <p className='project1caption'>Level up your gaming experience! This game recommender will help you find your next favorite game based on your previous play. This web app was developed using a python cosine similarity algorithm and the streamlit framework. </p>
-            <br></br>
-            <p><span className='projecttag6'>#python</span>&nbsp;&nbsp;&nbsp;<span className='projecttag7'>#machine learning</span>&nbsp;&nbsp;&nbsp;<span className='projecttag2'>#streamlit</span></p>
-            <br></br>
-            <p><a className='viewcodedemo' href="https://github.com/bk-kiran/gamerecommender" target='__blank'>Code</a>&nbsp;&nbsp;👨‍💻&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a className='viewcodedemo' href="https://gamerecommender-xioczcvbfed8mlpny35wni.streamlit.app/" target='__blank'>Live Demo</a>&nbsp;&nbsp;🖥️</p>
-        </div>
-        <div className='project2'>
-            <img className='project2screenshot' src={djproject}/>
-            <h1 className='project2title'>Predicting Stock Prices 📈</h1>
-            <br></br>
-            <p className='project2caption'>Predict future stock prices of the FAAMG companies! This app was developed using streamlit, a Random Forest Regression machine learning model and utilized web scraping to obtain stock data.</p>
+            <p className='project1caption'>Predict future stock prices of the FAAMG companies! This app was developed using streamlit, a Random Forest Regression machine learning model and utilized web scraping to obtain stock data.</p>
             <br></br>
             <p><span className='projecttag3'>#python</span>&nbsp;&nbsp;&nbsp;<span className='projecttag5'>#machine learning</span>&nbsp;&nbsp;&nbsp;<span className='projecttag4'>#web scraping</span></p>
             <br></br>
